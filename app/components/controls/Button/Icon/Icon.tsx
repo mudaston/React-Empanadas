5eaa6959e00ddda895e65199a5c092cc7ec8615f
Mudastone <twil.revaas@gmail.com>
2022-08-08 14:40:23 +0300

    update(Icon) add icon style and fontSize property

--- app/components/controls/Button/Icon/Icon.tsx
+++ app/components/controls/Button/Icon/Icon.tsx
@@ -1,13 +1,20 @@
 import { FC } from 'react'
 
+import style from './Icon.module.scss'
+
 interface OwnProps {
   children: JSX.Element
+  fontSize?: string
 }
 
 export type Props = OwnProps
 
-const Icon: FC<Props> = ({ children }) => {
-  return <>{children}</>
+const Icon: FC<Props> = ({ children, fontSize = '1rem' }) => {
+  return (
+    <span className={style['icon']} style={{ fontSize }}>
+      {children}
+    </span>
+  )
 }
 
 export default Icon
