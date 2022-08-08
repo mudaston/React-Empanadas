6e8353d7526dd8616f4b6e30f4131a1cea66d7ba
Mudastone <twil.revaas@gmail.com>
2022-08-08 14:42:21 +0300

    update(empanadas) add EmpanadaItem and EmpanadaList styles and logic

--- app/features/empanadas/EmpanadaItem/EmpanadaItem.tsx
+++ app/features/empanadas/EmpanadaItem/EmpanadaItem.tsx
@@ -1,5 +1,10 @@
 import { FC } from 'react'
 import Image from 'next/image'
+import { useTranslation } from 'next-i18next'
+
+import { Button } from '../../../components/controls'
+
+import style from './EmpanadaItem.module.scss'
 
 interface OwnProps {
   id: number
@@ -11,17 +16,36 @@ interface OwnProps {
 type Props = OwnProps
 
 const EmpanadaItem: FC<Props> = ({ id, name, price, image }) => {
+  const { t } = useTranslation(['currencies', 'home'])
+
   return (
-    <div>
-      <Image
-        src={image}
-        blurDataURL={image}
-        width={200}
-        height={200}
-        placeholder={'blur'}
-        alt={name}
-      />
-    </div>
+    <article className={style['empanada-item']}>
+      <div className={style['empanada-item__img']}>
+        <Image
+          src={image}
+          blurDataURL={image}
+          layout={'fill'}
+          placeholder={'blur'}
+          loading={'lazy'}
+          alt={name}
+        />
+      </div>
+      <div className={style['empanada-item__info-wrapper']}>
+        <span className={style['empanada-item__name']}>{name}</span>
+        <div className={style['empanada-item__info']}>
+          <span className={style['empanada-item__price']}>
+            от {price} {t('currencies:currency')}
+          </span>
+          <Button type={'add-item-button'}>
+            <Button.Icon fontSize='12px'>
+              <span className='icon-plus-icon' />
+            </Button.Icon>
+            <Button.LabelBold>{t('home:add')}</Button.LabelBold>
+            {/*<Button.Counter>{3}</Button.Counter>*/}
+          </Button>
+        </div>
+      </div>
+    </article>
   )
 }
 
