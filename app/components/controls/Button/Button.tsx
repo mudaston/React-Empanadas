cca6501641e0f02043460dcf37b85fa0fff38e0c
Mudastone <twil.revaas@gmail.com>
2022-08-12 21:16:48 +0300

    update(Button) add new button styles

--- app/components/controls/Button/Button.tsx
+++ app/components/controls/Button/Button.tsx
@@ -8,7 +8,14 @@ import Counter, { Props as CounterProps } from './Counter/Counter'
 
 import style from '../../../../styles/buttons/Button.module.scss'
 
-type ButtonTypes = 'none' | 'choose-filter-button' | 'add-item-button'
+type ButtonTypes =
+  | 'none'
+  | 'choose-filter-button'
+  | 'add-item-button'
+  | 'empty-basket-button'
+  | 'get-back-button-with-icon'
+  | 'get-back-button'
+  | 'pay-now-button'
 
 interface OwnProps {
   type?: ButtonTypes
@@ -34,6 +41,13 @@ const Button: FC<Props> & Subcomponents = ({ children, onClick, isActive, type }
         [style['choose-filter-button_active']]: type === 'choose-filter-button' && isActive,
         [style['add-item-button']]: type === 'add-item-button',
         [style['add-item-button_active']]: type === 'add-item-button' && isActive,
+        [style['empty-basket-button']]: type === 'empty-basket-button',
+        [style['empty-basket-button_active']]: type === 'empty-basket-button' && isActive,
+        [style['get-back-button-with-icon']]: type === 'get-back-button-with-icon',
+        [style['get-back-button-with-icon_active']]:
+          type === 'get-back-button-with-icon' && isActive,
+        [style['pay-now-button']]: type === 'pay-now-button',
+        [style['pay-now-button_active']]: type === 'pay-now-button' && isActive,
       })}
       onClick={() => onClick?.()}
     >
--- app/components/controls/Button/Counter/Counter.tsx
+++ app/components/controls/Button/Counter/Counter.tsx
@@ -1,21 +1,28 @@
 import { FC } from 'react'
+import cl from 'classnames'
 
 import style from './Counter.module.scss'
 
 interface OwnProps {
   children: number
+  show?: boolean
 }
 
 export type Props = OwnProps
 
-const Counter: FC<Props> = ({ children }) => {
+const Counter: FC<Props> = ({ children, show }) => {
   let fontSize = '13px'
 
   if (children >= 10) fontSize = '11px'
   if (children >= 100) fontSize = '10px'
 
   return (
-    <div className={style['counter']} style={{ fontSize }}>
+    <div
+      className={cl(style['counter'], {
+        [style['counter_active']]: show,
+      })}
+      style={{ fontSize }}
+    >
       <span className={style['counter__number']}>{children}</span>
     </div>
   )
