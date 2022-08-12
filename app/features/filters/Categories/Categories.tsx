6900988f19a9978c98d9540e66f262840e80c7af
Mudastone <twil.revaas@gmail.com>
2022-08-12 21:21:29 +0300

    update(Categories) add useTransition hook, update Categories style

--- app/features/filters/Categories/Categories.tsx
+++ app/features/filters/Categories/Categories.tsx
@@ -1,9 +1,12 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState, useTransition } from 'react'
+import { useDispatch } from 'react-redux'
 import { useRouter } from 'next/router'
 
 import type { LocaleType } from '../../../../interfaces'
 
 import { useGetCategoriesQuery } from './categories-service'
+import { setCategoryFilter } from '../../../redux/slices/filters-slice'
+import { categoryFilters } from '../../../helpers/category-filters'
 
 import { Button } from '../../../components/controls'
 
@@ -17,11 +20,21 @@ const Categories: FC<Props> = (props) => {
   const { locale } = useRouter()
   const { data } = useGetCategoriesQuery({ locale: locale as LocaleType })
   const [currentCategory, setCurrentCategory] = useState(data?.data[0].label)
+  const [currentValue, setCurrentValue] = useState(data?.data[0].value)
+  const [, startTransition] = useTransition()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(setCategoryFilter(currentValue ?? categoryFilters.ALL))
+  }, [currentValue])
 
   const chooseCategoryHandler = (label: string, value: string) => {
     if (label === currentCategory) return
 
-    setCurrentCategory(label)
+    startTransition(() => {
+      setCurrentCategory(label)
+      setCurrentValue(value)
+    })
   }
 
   return (
