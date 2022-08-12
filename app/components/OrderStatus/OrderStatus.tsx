928f9932e16499926370c9fdc9b2cedc704e921e
Mudastone <twil.revaas@gmail.com>
2022-08-12 21:14:46 +0300

    update(OrderStatus) replace div with Link component

--- app/components/OrderStatus/OrderStatus.tsx
+++ app/components/OrderStatus/OrderStatus.tsx
@@ -1,6 +1,12 @@
 import { FC, useEffect, useState } from 'react'
+import Link from 'next/link'
+import { useSelector } from 'react-redux'
 import { useTranslation } from 'next-i18next'
 import ReactTooltip from 'react-tooltip'
+
+import { basketItemsAmount, basketOrderSum } from '../../redux/selectors/basket'
+// import { useExchangeCurrency } from '../../hooks/useExchangeCurrency'
+import { routerPaths } from '../../helpers/router-paths'
 
 import style from './OrderStatus.module.scss'
 import colors from '../../../styles/colors.module.scss'
@@ -12,6 +18,9 @@ type Props = OwnProps
 const OrderStatus: FC<Props> = (props) => {
   const { t } = useTranslation(['currencies', 'order-status'])
   const [showToolTip, setShowToolTip] = useState(false)
+  const itemsAmount = useSelector(basketItemsAmount)
+  const orderSum = useSelector(basketOrderSum)
+  // const { exchangeCurrency } = useExchangeCurrency()
 
   useEffect(() => {
     setShowToolTip(true)
@@ -22,49 +31,52 @@ const OrderStatus: FC<Props> = (props) => {
   }, [])
 
   return (
-    <div className={style['order-status']}>
-      <span
-        className={style['order-status__sum']}
-        data-tip={t('order-status:order_price')}
-        data-for='order-sum'
-      >
-        520 {t('currencies:currency')}
-      </span>
-      <div className={style['order-status__divider']} />
-      <div
-        className={`${style['order-status__shopping-cart']}`}
-        data-tip={t('order-status:number_of_items_in_order')}
-        data-for='shopping-cart'
-      >
-        <span className={'icon-shopping-cart'} />3
-      </div>
-      {showToolTip && (
-        <>
-          <ReactTooltip
-            id='order-sum'
-            textColor={colors.textColorSecondary}
-            backgroundColor={colors.tooltipBackgroundColor}
-            type={'dark'}
-            effect={'solid'}
-            place={'bottom'}
-            delayShow={200}
-            delayHide={200}
-            clickable={true}
-          />
-          <ReactTooltip
-            id='shopping-cart'
-            textColor={colors.textColorSecondary}
-            backgroundColor={colors.tooltipBackgroundColor}
-            type={'dark'}
-            effect={'solid'}
-            place={'bottom'}
-            delayShow={200}
-            delayHide={200}
-            clickable={true}
-          />
-        </>
-      )}
-    </div>
+    <Link href={routerPaths.order} scroll={false}>
+      <a className={style['order-status']}>
+        <span
+          className={style['order-status__sum']}
+          data-tip={t('order-status:order_price')}
+          data-for='order-sum'
+        >
+          {orderSum} {t('currencies:currency')}
+        </span>
+        <div className={style['order-status__divider']} />
+        <div
+          className={`${style['order-status__shopping-cart']}`}
+          data-tip={t('order-status:number_of_items_in_order')}
+          data-for='shopping-cart'
+        >
+          <span className={'icon-shopping-cart'} />
+          {itemsAmount}
+        </div>
+        {showToolTip && (
+          <>
+            <ReactTooltip
+              id='order-sum'
+              textColor={colors.textColorSecondary}
+              backgroundColor={colors.tooltipBackgroundColor}
+              type={'dark'}
+              effect={'solid'}
+              place={'bottom'}
+              delayShow={200}
+              delayHide={200}
+              clickable={true}
+            />
+            <ReactTooltip
+              id='shopping-cart'
+              textColor={colors.textColorSecondary}
+              backgroundColor={colors.tooltipBackgroundColor}
+              type={'dark'}
+              effect={'solid'}
+              place={'bottom'}
+              delayShow={200}
+              delayHide={200}
+              clickable={true}
+            />
+          </>
+        )}
+      </a>
+    </Link>
   )
 }
 
