045eb9f65b9fb6f61be7e731d6a9c2eca236f2c6
Mudastone <twil.revaas@gmail.com>
2022-08-12 21:25:23 +0300

    update(store/index.ts) add new reducers

--- app/redux/store/index.ts
+++ app/redux/store/index.ts
@@ -4,6 +4,10 @@ import { createWrapper } from 'next-redux-wrapper'
 import { sortApi } from '../../features/filters/Sort/sort-service'
 import { categoriesApi } from '../../features/filters/Categories/categories-service'
 import { empanadasApi } from '../../features/empanadas/empanadas-service'
+import { orderApi } from '../apis/order-api'
+import { currencyExchangeApi } from '../apis/currency-exchange-api'
+import { filtersSlice } from '../slices/filters-slice'
+import { basketSlice } from '../slices/basket-slice'
 
 export const makeStore = () =>
   configureStore({
@@ -11,12 +15,18 @@ export const makeStore = () =>
       [sortApi.reducerPath]: sortApi.reducer,
       [categoriesApi.reducerPath]: categoriesApi.reducer,
       [empanadasApi.reducerPath]: empanadasApi.reducer,
+      [orderApi.reducerPath]: orderApi.reducer,
+      [currencyExchangeApi.reducerPath]: currencyExchangeApi.reducer,
+      [filtersSlice.name]: filtersSlice.reducer,
+      [basketSlice.name]: basketSlice.reducer,
     },
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(
         sortApi.middleware,
         categoriesApi.middleware,
-        empanadasApi.middleware
+        empanadasApi.middleware,
+        orderApi.middleware,
+        currencyExchangeApi.middleware
       ),
     devTools: process.env.NODE_ENV !== 'production',
   })
