e7b7b8d073474eeefa90b8fb0564ce4f6173fe36
Mudastone <twil.revaas@gmail.com>
2022-08-12 21:24:02 +0300

    update(api-paths.ts) add new paths

--- app/helpers/api-paths.ts
+++ app/helpers/api-paths.ts
@@ -2,4 +2,6 @@ export enum apiPaths {
   getSortFilters = 'filters/sort',
   getCategoriesFilters = 'filters/categories',
   getEmpanadas = 'empanadas',
+  getItemsByIds = 'order/items-by-ids',
+  currencyExchange = 'currency-exchange',
 }
