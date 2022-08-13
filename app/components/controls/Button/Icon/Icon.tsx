50ebbdb1b7876f352c8b23f6e85b1e56ae5c7775
Mudastone <twil.revaas@gmail.com>
2022-08-13 16:18:04 +0300

    update(Button/Icon) add Icon weightless type, transform property

--- app/components/controls/Button/Icon/Icon.tsx
+++ app/components/controls/Button/Icon/Icon.tsx
@@ -2,18 +2,29 @@ import { FC } from 'react'
 
 import style from './Icon.module.scss'
 
+type IconTypes = 'weightless'
+
 interface OwnProps {
   children: JSX.Element
   fontSize?: string
+  type?: IconTypes
+  transform?: string
 }
 
 export type Props = OwnProps
 
-const Icon: FC<Props> = ({ children, fontSize = '1rem' }) => {
+const Icon: FC<Props> = ({ children, fontSize = '1rem', type, transform }) => {
   return (
-    <span className={style['icon']} style={{ fontSize }}>
-      {children}
-    </span>
+    <div
+      className={style['icon']}
+      style={{
+        fontSize,
+        padding: type !== 'weightless' ? `0 calc(${fontSize} / 2)` : '',
+        transform,
+      }}
+    >
+      <div className={style['icon__children']}>{children}</div>
+    </div>
   )
 }
 
