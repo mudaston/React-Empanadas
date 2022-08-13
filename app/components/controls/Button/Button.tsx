d849d5b6c4be1087fae1c7872350a37be4ac9ce8
Mudastone <twil.revaas@gmail.com>
2022-08-13 16:16:06 +0300

    update(Button) add new styles, replace cn object with few rules

--- app/components/controls/Button/Button.tsx
+++ app/components/controls/Button/Button.tsx
@@ -16,6 +16,8 @@ type ButtonTypes =
   | 'get-back-button-with-icon'
   | 'get-back-button'
   | 'pay-now-button'
+  | 'rounded-button-orange'
+  | 'rounded-button-grey'
 
 interface OwnProps {
   type?: ButtonTypes
@@ -37,17 +39,8 @@ const Button: FC<Props> & Subcomponents = ({ children, onClick, isActive, type }
   return (
     <button
       className={cn(style['base-button'], {
-        [style['choose-filter-button']]: type === 'choose-filter-button',
-        [style['choose-filter-button_active']]: type === 'choose-filter-button' && isActive,
-        [style['add-item-button']]: type === 'add-item-button',
-        [style['add-item-button_active']]: type === 'add-item-button' && isActive,
-        [style['empty-basket-button']]: type === 'empty-basket-button',
-        [style['empty-basket-button_active']]: type === 'empty-basket-button' && isActive,
-        [style['get-back-button-with-icon']]: type === 'get-back-button-with-icon',
-        [style['get-back-button-with-icon_active']]:
-          type === 'get-back-button-with-icon' && isActive,
-        [style['pay-now-button']]: type === 'pay-now-button',
-        [style['pay-now-button_active']]: type === 'pay-now-button' && isActive,
+        [style[String(type)]]: type,
+        [style[`${type}_active`]]: type && isActive,
       })}
       onClick={() => onClick?.()}
     >
