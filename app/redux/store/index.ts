ab2c5ce131fff8c6c83a85bff554e0d48632de35
Mudastone <twil.revaas@gmail.com>
2022-08-13 23:14:30 +0300

    update(store/index.ts) remove orderApi reducer

--- app/redux/store/index.ts
+++ app/redux/store/index.ts
@@ -4,7 +4,6 @@ import { createWrapper } from 'next-redux-wrapper'
 import { sortApi } from '../../features/filters/Sort/sort-service'
 import { categoriesApi } from '../../features/filters/Categories/categories-service'
 import { empanadasApi } from '../../features/empanadas/empanadas-service'
-import { orderApi } from '../apis/order-api'
 import { currencyExchangeApi } from '../apis/currency-exchange-api'
 import { filtersSlice } from '../slices/filters-slice'
 import { basketSlice } from '../slices/basket-slice'
@@ -15,7 +14,6 @@ export const makeStore = () =>
       [sortApi.reducerPath]: sortApi.reducer,
       [categoriesApi.reducerPath]: categoriesApi.reducer,
       [empanadasApi.reducerPath]: empanadasApi.reducer,
-      [orderApi.reducerPath]: orderApi.reducer,
       [currencyExchangeApi.reducerPath]: currencyExchangeApi.reducer,
       [filtersSlice.name]: filtersSlice.reducer,
       [basketSlice.name]: basketSlice.reducer,
@@ -25,7 +23,6 @@ export const makeStore = () =>
         sortApi.middleware,
         categoriesApi.middleware,
         empanadasApi.middleware,
-        orderApi.middleware,
         currencyExchangeApi.middleware
       ),
     devTools: process.env.NODE_ENV !== 'production',
