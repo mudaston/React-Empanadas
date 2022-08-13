a6796eccde1e8a3bf24401edd9ab9654bb65c90f
Mudastone <twil.revaas@gmail.com>
2022-08-13 23:08:01 +0300

    update(EmpanadasOrder/EmpanadasOrderItem.tsx) add ariaLabel property

--- app/features/empanadasOrder/EmpanadasOrderItem/EmpanadasOrderItem.tsx
+++ app/features/empanadasOrder/EmpanadasOrderItem/EmpanadasOrderItem.tsx
@@ -6,27 +6,31 @@ import { Button } from '../../../components/controls'
 import style from './EmpanadasOrderItem.module.scss'
 import image from '../../../assets/img/logo.png'
 
-interface OwnProps {}
+interface OwnProps {
+  id?: number
+  name?: string
+  image?: string
+}
 
 type Props = OwnProps
 
-const EmpanadasOrderItem: FC<Props> = (props) => {
+const EmpanadasOrderItem: FC<Props> = ({ id, name }) => {
   return (
     <article className={style['empanada-order-item']}>
       <header className={style['empanada-order-item__header']}>
         <div className={style['empanada-order-item__image']}>
-          <Image src={image} layout={'fill'} />
+          <Image src={image} layout={'fill'} alt={name} />
         </div>
         <span className={style['empanada-order-item__title']}>Сырный цыпленок</span>
       </header>
       <main className={style['empanada-order-item__main']}>
-        <Button type={'rounded-button-orange'}>
+        <Button ariaLabel={'increment item'} type={'rounded-button-orange'}>
           <Button.Icon type={'weightless'} fontSize={'2px'}>
             <span className='icon-minus-icon' />
           </Button.Icon>
         </Button>
         <span className={style['empanada-order-item__amount']}>3</span>
-        <Button type={'rounded-button-orange'}>
+        <Button ariaLabel={'decrement item'} type={'rounded-button-orange'}>
           <Button.Icon type={'weightless'} fontSize={'11px'}>
             <span className='icon-plus-icon' />
           </Button.Icon>
@@ -34,8 +38,8 @@ const EmpanadasOrderItem: FC<Props> = (props) => {
       </main>
       <footer className={style['empanada-order-item__footer']}>
         <span className={style['empanada-order-item__sum']}>770 Р</span>
-        <Button type={'rounded-button-grey'}>
-          <Button.Icon transform={'rotate(45deg)'} type={'weightless'} fontSize={'11px'}>
+        <Button ariaLabel={'delete item'} type={'rounded-button-grey'}>
+          <Button.Icon type={'weightless'} transform={'rotate(45deg)'} fontSize={'11px'}>
             <span className='icon-plus-icon' />
           </Button.Icon>
         </Button>
