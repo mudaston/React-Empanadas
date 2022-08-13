8dc2ce012e98cf78bb7cf3c0e52ccd555d86f64e
Mudastone <twil.revaas@gmail.com>
2022-08-13 23:09:22 +0300

    update(helpers/api-paths.ts) rename property

--- app/helpers/api-paths.ts
+++ app/helpers/api-paths.ts
@@ -2,6 +2,6 @@ export enum apiPaths {
   getSortFilters = 'filters/sort',
   getCategoriesFilters = 'filters/categories',
   getEmpanadas = 'empanadas',
-  getItemsByIds = 'order/empanadas-by-id',
+  getEmpanadasByID = 'order/empanadas-by-id',
   currencyExchange = 'currency-exchange',
 }
