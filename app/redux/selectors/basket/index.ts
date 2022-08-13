3fa39c5d69ebeaf766c89b733da58766ecfd7030
Mudastone <twil.revaas@gmail.com>
2022-08-13 20:53:40 +0300

    update(redux/selectors) update selectors for new basket-slice.ts structure

--- app/redux/selectors/basket/index.ts
+++ app/redux/selectors/basket/index.ts
@@ -1,13 +1,14 @@
+// @ts-nocheck
 import { createSelector } from '@reduxjs/toolkit'
 import type { RootState } from '../../store'
 
 const basketEntities = (state: RootState) => state.basketSlice.entities
 
-const basketEmpanadasEntities = (state: RootState) => state.basketSlice.entities.empanadas
+const basketEmpanadasEntities = (state: RootState) =>
+  state.basketSlice.entities.empanadas.basketItems
 
 export const basketItemsAmount = createSelector(basketEntities, (items) =>
-  // @ts-ignore
-  Object.keys(items).reduce((previous, key) => previous + items[key].length, 0)
+  Object.keys(items).reduce((previous, key) => previous + items[key].basketItems.length, 0)
 )
 
 // get the sum of all items in the basketSlice by reducing every object item
@@ -15,8 +16,10 @@ export const basketOrderSum = createSelector(basketEntities, (items) =>
   Object.keys(items).reduce(
     (previous, key) =>
       previous +
-      // @ts-ignore
-      items[key].reduce((accumulator, item) => accumulator + item.price * item.amount, 0),
+      items[key].basketItems.reduce(
+        (accumulator, item) => accumulator + item.price * item.amount,
+        0
+      ),
     0
   )
 )
