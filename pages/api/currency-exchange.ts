36a27c0c84759da47f2819ddb2cbf6bf6d65ba57
Mudastone <twil.revaas@gmail.com>
2022-09-04 13:23:32 +0300

    fix

--- pages/api/currency-exchange.ts
+++ pages/api/currency-exchange.ts
@@ -1,14 +1,35 @@
 import axios from 'axios'
+import Cors from 'cors'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {
   ICurrencyExchangeResponse,
   ICurrencyExchangeServerResponse,
 } from '../../interfaces/ICurrencyExchange'
 
+const cors = Cors({
+  methods: ['GET'],
+})
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse<ICurrencyExchangeResponse>,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) return reject(result)
+
+      return resolve(result)
+    })
+  })
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ICurrencyExchangeResponse>
 ) {
+  await runMiddleware(req, res, cors)
+
   try {
     const { data } = await axios.get<ICurrencyExchangeServerResponse>(
       'https://v6.exchangerate-api.com/v6/26c993debdc06d68aa5521c1/latest/USD'
