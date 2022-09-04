654782208b1ccbe6523438843cb6ebe836049dda
Mudastone <twil.revaas@gmail.com>
2022-09-04 12:59:10 +0300

    update(selectors/basket/index.ts) add a few empanadas selectors

--- app/redux/selectors/basket/index.ts
+++ app/redux/selectors/basket/index.ts
@@ -7,9 +7,18 @@ const basketEntities = (state: RootState) => state.basketSlice.entities
 const basketEmpanadasEntities = (state: RootState) =>
   state.basketSlice.entities.empanadas.basketItems
 
+const orderEmpanadasEntities = (state: RootState) => state.basketSlice.entities.empanadas.orderItems
+
 export const basketItemsAmount = createSelector(basketEntities, (items) =>
   Object.keys(items).reduce((previous, key) => previous + items[key].basketItems.length, 0)
 )
+
+export const empanadasAmount = createSelector(basketEmpanadasEntities, (items) =>
+  items.reduce((acc, currentValue) => acc + currentValue.amount, 0)
+)
+
+export const empanadasOrderItems = createSelector(orderEmpanadasEntities, (items) => items)
+export const empanadasBasketItems = createSelector(basketEmpanadasEntities, (items) => items)
 
 // get the sum of all items in the basketSlice by reducing every object item
 export const basketOrderSum = createSelector(basketEntities, (items) =>
@@ -26,3 +35,10 @@ export const basketOrderSum = createSelector(basketEntities, (items) =>
 
 export const empanadaAmountByID = (id: number) =>
   createSelector(basketEmpanadasEntities, (items) => items.find((item) => item.id === id)?.amount)
+
+export const empanadaCostByID = (id: number) =>
+  createSelector(basketEmpanadasEntities, (items) => {
+    const empanada = items.find((item) => item.id === id)
+
+    return empanada?.amount * empanada?.price
+  })
