c4853bcad9a27443f906abb60aaaca2d642554b9
Mudastone <twil.revaas@gmail.com>
2022-09-04 12:55:08 +0300

    update(EmpanadasOrderList.tsx) add empanadas async fetch

--- app/features/empanadasOrder/EmpanadasOrderList/EmpanadasOrderList.tsx
+++ app/features/empanadasOrder/EmpanadasOrderList/EmpanadasOrderList.tsx
@@ -1,4 +1,10 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
+
+import { useAppDispatch } from '../../../hooks/useAppDispatch'
+import { fetchEmpanadas } from '../../../redux/slices/basket-slice'
+import { empanadasBasketItems, empanadasOrderItems } from '../../../redux/selectors/basket'
 
 import EmpanadasOrderItem from '../EmpanadasOrderItem/EmpanadasOrderItem'
 
@@ -9,17 +15,28 @@ interface OwnProps {}
 type Props = OwnProps
 
 const EmpanadasOrderList: FC<Props> = () => {
+  const empanadasOrder = useSelector(empanadasOrderItems)
+  const empanadasBasket = useSelector(empanadasBasketItems)
+  const { locale, defaultLocale } = useRouter()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const ids = empanadasBasket.map(({ id }) => id)
+
+    fetchEmpanadasAsync(ids)
+  }, [])
+
+  const fetchEmpanadasAsync = async (ids: number[]) => {
+    await dispatch(fetchEmpanadas({ ids, locale: locale || defaultLocale || 'uk' }))
+  }
+
   return (
     <ul className={style['empanadas-order-list']}>
-      <li className={style['empanadas-order-list__li']}>
-        <EmpanadasOrderItem />
-      </li>
-      <li className={style['empanadas-order-list__li']}>
-        <EmpanadasOrderItem />
-      </li>
-      <li className={style['empanadas-order-list__li']}>
-        <EmpanadasOrderItem />
-      </li>
+      {empanadasOrder.map(({ id, label, image }) => (
+        <li key={id} className={style['empanadas-order-list__li']}>
+          <EmpanadasOrderItem id={id} name={label} image={image} />
+        </li>
+      ))}
     </ul>
   )
 }
