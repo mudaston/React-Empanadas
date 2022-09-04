93396da5a15e45d9da5e926c2433fcf021abb751
Mudastone <twil.revaas@gmail.com>
2022-09-04 12:57:42 +0300

    update(EmpanadasOrderItem.tsx) add basket dispatch actions

--- app/features/empanadasOrder/EmpanadasOrderItem/EmpanadasOrderItem.tsx
+++ app/features/empanadasOrder/EmpanadasOrderItem/EmpanadasOrderItem.tsx
@@ -1,44 +1,74 @@
 import { FC } from 'react'
 import Image from 'next/image'
+import { useTranslation } from 'next-i18next'
+
+import { useAppDispatch } from '../../../hooks/useAppDispatch'
+import { useAppSelector } from '../../../hooks/useAppSelector'
+import {
+  decrementEmpanadaByID,
+  deleteEmpanadaByID,
+  incrementEmpanadaByID,
+} from '../../../redux/slices/basket-slice'
+import { empanadaAmountByID, empanadaCostByID } from '../../../redux/selectors/basket'
+import { useExchangeCurrency } from '../../../hooks/useExchangeCurrency'
 
 import { Button } from '../../../components/controls'
 
 import style from './EmpanadasOrderItem.module.scss'
-import image from '../../../assets/img/logo.png'
 
 interface OwnProps {
-  id?: number
-  name?: string
-  image?: string
+  id: number
+  name: string
+  image: string
 }
 
 type Props = OwnProps
 
-const EmpanadasOrderItem: FC<Props> = ({ id, name }) => {
+const EmpanadasOrderItem: FC<Props> = ({ id, name, image }) => {
+  const dispatch = useAppDispatch()
+  const { t } = useTranslation(['currencies'])
+  const empanadaAmount = useAppSelector(empanadaAmountByID(id))
+  const cost = useAppSelector(empanadaCostByID(id))
+  const { exchangeCurrency } = useExchangeCurrency()
+
   return (
     <article className={style['empanada-order-item']}>
       <header className={style['empanada-order-item__header']}>
         <div className={style['empanada-order-item__image']}>
           <Image src={image} layout={'fill'} alt={name} />
         </div>
-        <span className={style['empanada-order-item__title']}>Сырный цыпленок</span>
+        <span className={style['empanada-order-item__title']}>{name}</span>
       </header>
       <main className={style['empanada-order-item__main']}>
-        <Button ariaLabel={'increment item'} type={'rounded-button-orange'}>
+        <Button
+          ariaLabel={'increment item'}
+          type={'rounded-button-orange'}
+          onClick={() => dispatch(decrementEmpanadaByID(id))}
+        >
           <Button.Icon type={'weightless'} fontSize={'2px'}>
             <span className='icon-minus-icon' />
           </Button.Icon>
         </Button>
-        <span className={style['empanada-order-item__amount']}>3</span>
-        <Button ariaLabel={'decrement item'} type={'rounded-button-orange'}>
+        <span className={style['empanada-order-item__amount']}>{empanadaAmount}</span>
+        <Button
+          ariaLabel={'decrement item'}
+          type={'rounded-button-orange'}
+          onClick={() => dispatch(incrementEmpanadaByID(id))}
+        >
           <Button.Icon type={'weightless'} fontSize={'11px'}>
             <span className='icon-plus-icon' />
           </Button.Icon>
         </Button>
       </main>
       <footer className={style['empanada-order-item__footer']}>
-        <span className={style['empanada-order-item__sum']}>770 Р</span>
-        <Button ariaLabel={'delete item'} type={'rounded-button-grey'}>
+        <span className={style['empanada-order-item__sum']}>
+          {exchangeCurrency(cost)} {t('currencies:currency')}
+        </span>
+        <Button
+          ariaLabel={'delete item'}
+          type={'rounded-button-grey'}
+          onClick={() => dispatch(deleteEmpanadaByID(id))}
+        >
           <Button.Icon type={'weightless'} transform={'rotate(45deg)'} fontSize={'11px'}>
             <span className='icon-plus-icon' />
           </Button.Icon>
