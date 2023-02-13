7ec3c52d15de8f42546bb577ce90016bfdb5cbd4
Renat Vasilenko <64277973+mudaston@users.noreply.github.com>
2023-02-13 21:56:22 +0200

    save

--- services/axios-worker.ts
+++ services/axios-worker.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosWorker = axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://react-empanadas-4a06wown3-mudaston.vercel.app/',
   timeout: 10000,
 })
