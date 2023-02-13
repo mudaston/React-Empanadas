116b49616b5c72b58c4430fdef9e6e3e785d84af
Renat Vasilenko <64277973+mudaston@users.noreply.github.com>
2023-02-13 21:38:06 +0200

    save

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,11 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     locales: ['en', 'uk', 'ru'],
     localeDetection: true,
     defaultLocale: 'uk',
+    localePath: path.resolve('locales'),
+    localeStructure: '{{lng}}/{{ns}}',
   },
 }
