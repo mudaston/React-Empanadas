ba63ffefe729a9889afec44c3f4ba71f06f87ec3
Renat Vasilenko <64277973+mudaston@users.noreply.github.com>
2023-02-13 21:52:01 +0200

    save

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     API_URL:
       process.env.NODE_ENV !== 'production'
         ? 'http://127.0.0.1:3000/api'
-        : 'http://127.0.0.1:3000/api',
+        : 'https://react-empanadas-4a06wown3-mudaston.vercel.app/api',
     CURRENT_LOCALE: i18n.defaultLocale,
   },
 }
