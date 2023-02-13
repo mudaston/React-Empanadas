9ec2705e18a2077eab7a42de938fb389476e49c2
Renat Vasilenko <64277973+mudaston@users.noreply.github.com>
2023-02-13 22:06:26 +0200

    save

--- next.config.js
+++ next.config.js
@@ -15,23 +15,6 @@ const nextConfig = {
         : 'https://react-empanadas-4a06wown3-mudaston.vercel.app/api',
     CURRENT_LOCALE: i18n.defaultLocale,
   },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
